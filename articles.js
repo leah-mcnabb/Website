bcfee5a81a7d76c129c2bb4044582467c6315bf8
blue-or-pink <jane.carol.berry@gmail.com>
2026-09-06 23:08:10 -0400

    added a draft of the article page w the first article

---
+++ articles.js
@@ -0,0 +1,10 @@
+// const titles = (
+//     {
+//     title: "Introduction to Linguistics",
+//     date : "August 31st"
+//     }
+// )
+
+// for (i = 0; i < Object.keys(titles).length; i ++) {
+//     let arrow = document.createElement("button");
+// }
